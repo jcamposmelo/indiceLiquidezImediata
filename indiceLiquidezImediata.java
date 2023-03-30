87f84ca6fe8ef31a5d70f01ea8071232f7daa0bf
jcamposmelo <jcamposmelo@yahoo.com.br>
2023-03-30 15:28:01 -0300

    Como calcular o Índice de Liquidez Imediata?

---
+++ indiceLiquidezImediata.java
@@ -0,0 +1,14 @@
+public class liquidezImediata {
+	public static void main(String[] args) {
+		double disponibilidades = 5889000;
+		double passivoCirculante = 27503000;
+
+		double liquidezImediata;
+
+		liquidezImediata = disponibilidades / passivoCirculante;
+
+		System.out.println("O índice calculado em liquidez Imediata é?  " + 
+				liquidezImediata);
+	}
+
+}
